dfac3a7db5b6afdd9f9586edebe7bcc3042e9bbb
baldur <baldur@siegels.us>
2022-11-22 19:35:02 +0100

    created basic class

--- musikloader.py
+++ musikloader.py
@@ -1,1 +1,29 @@
-print('horden')
+from pathlib import Path
+
+class musikloader:
+    def __init__(self, url: str):
+        """ 
+        constructor 
+        str url: the channel url to be downloaded
+        """
+        self.url = url
+
+    def update_url(self, url: str):
+        """
+        updates the channel url
+        str url: the channel url
+        """
+        self.url = url
+    
+    def download(self, path: str = './download'):
+        """
+        starts the download progress
+        str path: the target directory to download files to
+        """
+        base_dir = Path(path)
+        print(base_dir)
+
+
+if __name__ == "__main__":
+    ml = musikloader("horden")
+    ml.download()
